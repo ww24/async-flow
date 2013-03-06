e3dc24896f80791054191d52a78ab1188efc64c3
ww24 <ww24gm@gmail.com>
2013-03-07 08:40:19 +0900

    fix argument passing bug

--- test/async-flow.js
+++ test/async-flow.js
@@ -11,7 +11,7 @@ describe("async-flow", function () {
       setTimeout(function () {
         results.push(1);
         parallelA.results[0].should.to.equal(1);
-        next();
+        next("ok?");
       }, 50);
       return 1;
     });
@@ -36,9 +36,23 @@ describe("async-flow", function () {
       return 3;
     });
     
-    flow.create(parallelA, parallelB, parallelC)
+    var parallelFlow = flow.create(parallelA, parallelB, parallelC)
       .flow(function (next, skip) {
         results.should.to.eql([1, 3, 2]);
+        next("testA");
+      });
+    
+    parallelA.flow(function (next, skip, arg1) {
+      arg1.should.to.equal("ok?");
+      setTimeout(function () {
+        next("testB");
+      }, 100);
+    });
+    
+    flow.create(parallelFlow, parallelA)
+      .flow(function (next, skip, arg1, arg2) {
+        arg1.should.to.equal("testA");
+        arg2.should.to.equal("testB");
         next();
         done();
       });
@@ -52,29 +66,56 @@ describe("async-flow", function () {
       setTimeout(function () {
         results.push(1);
         sequential.results[0].should.to.equal(1);
-        next();
+        next("hoge");
       }, 50);
       return 1;
     });
-    sequential.flow(function (next, skip) {
+    sequential.flow(function (next, skip, arg1) {
       setTimeout(function () {
         results.push(2);
         sequential.results[1].should.to.equal(2);
-        next();
+        arg1.should.to.equal("hoge");
+        next(arg1, 200);
       }, 150);
       return 2;
     });
-    sequential.flow(function (next, skip) {
+    sequential.flow(function (next, skip, arg1, arg2) {
       setTimeout(function () {
         results.push(3);
         sequential.results[2].should.to.equal(3);
-        next();
+        arg1.should.to.equal("hoge");
+        arg2.should.to.equal(200);
+        next(arg1, arg2, ["array"]);
       }, 100);
       return 3;
     });
-    sequential.flow(function (next, skip) {
+    sequential.flow(function (next, skip, arg1, arg2, arg3) {
       results.should.to.eql([1, 2, 3]);
+      arg1.should.to.equal("hoge");
+      arg2.should.to.equal(200);
+      arg3.should.to.eql(["array"]);
       next();
+      done();
+    });
+  });
+  
+  it("sequential skip", function (done) {
+    var sequential = flow.create();
+    sequential.flow(function (next, skip) {
+      setTimeout(function () {
+        skip(2, "skip");
+      }, 200);
+    });
+    sequential.flow(function (next, skip) {
+      setTimeout(function () {
+        next();
+      }, 100);
+    });
+    sequential.flow(function (next, skip) {
+      next();
+    });
+    sequential.flow(function (next, skip, arg1) {
+      arg1.should.to.equal("skip");
       done();
     });
   });
